522bfd21b347e63fcd816565d12f8e381d787755
Hey <mathieumontmail@gmail.com>
2021-05-04 01:34:58 +0200

    Hello

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -27,7 +27,7 @@ class HomeController extends AbstractController
    * @Route("/home")
    */
   public function homepage(){
-    $message = "Hello fdp bis";
+    $message = "Hello fdp bis zebi";
     return $this->render('home/index.html.twig', [
       'number' => 16,
       'message' => $message
