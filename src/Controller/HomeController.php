5bb85b790cba770a784203f2abc8d90d3e72f62f
Mathieu <f3f00cb0@gmail.com>
2021-05-08 23:02:53 +0200

    'modif'

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Finder\Finder;
 
 class HomeController extends AbstractController
 {
@@ -16,6 +17,13 @@ class HomeController extends AbstractController
       $number = 15;
 
       $message = "Hello fdp";
+
+      $finder = new Finder();
+      // find all files in the current directory
+      $finder->files()->in(__DIR__);
+
+      var_dump($finder);
+      die;
 
       return $this->render('home/index.html.twig', [
           'number' => $number,
