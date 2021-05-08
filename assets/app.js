0ca0d4ba4e87a2d5496cc6708d71738c471285ae
Mathieu <f3f00cb0@gmail.com>
2021-05-08 23:46:34 +0200

    'm'

--- assets/app.js
+++ assets/app.js
@@ -10,3 +10,25 @@ import './styles/app.css';
 
 // start the Stimulus application
 import './bootstrap';
+
+// PRESS THE MENU BUTTON TO TRIGGER ANIMATION
+// PRESS PLAY BUTTON TO LISTEN THE DEMO SONG
+
+// As seen on: "https://dribbble.com/shots/2144866-Day-5-Music-Player-Rebound/"
+
+// THANK YOU!
+
+var audio = document.getElementById('audio');
+var playpause = document.getElementById("play");
+
+
+function togglePlayPause() {
+   if (audio.paused || audio.ended) {
+      playpause.title = "Pause";
+      audio.play();
+   } else {
+      playpause.title = "Play";
+      audio.pause();
+   }
+}
+
