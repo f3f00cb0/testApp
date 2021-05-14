5c543a1495e05aa5569ff6c71c8e68e724723467
Ubuntu <ubuntu@vps-306e9730.vps.ovh.net>
2021-05-14 15:04:01 +0000

    ajout entité Titre

---
+++ src/Entity/Titre.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TitreRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TitreRepository::class)
+ */
+class Titre
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $path;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $album;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $artiste;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $n;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): self
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    public function getAlbum(): ?int
+    {
+        return $this->album;
+    }
+
+    public function setAlbum(int $album): self
+    {
+        $this->album = $album;
+
+        return $this;
+    }
+
+    public function getArtiste(): ?int
+    {
+        return $this->artiste;
+    }
+
+    public function setArtiste(int $artiste): self
+    {
+        $this->artiste = $artiste;
+
+        return $this;
+    }
+
+    public function getN(): ?string
+    {
+        return $this->n;
+    }
+
+    public function setN(string $n): self
+    {
+        $this->n = $n;
+
+        return $this;
+    }
+}
