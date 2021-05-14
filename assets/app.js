497793123b44131089aa9622c293f57ee1fdedb6
Mathieu <f3f00cb0@gmail.com>
2021-05-14 15:50:30 +0200

    Modification fichier init fichier js

--- assets/app.js
+++ assets/app.js
@@ -11,48 +11,50 @@ import './styles/app.css';
 // start the Stimulus application
 import './bootstrap';
 
-
-class Randomize {
+window.onload = function() {
+    class Randomize {
             
-    constructor(){
-       
-
-        this.init()
-        this.onClick()
+        constructor(){
+           
+    
+            this.init()
+            this.onClick()
+        }
+    
+        init(){
+            const images = [
+                {id: 0, url: './images/grimace1.png'},
+                {id: 1, url: './images/grimace2.png'},
+                {id: 2, url: './images/grimace3.png'},
+                {id: 3, url: './images/grimace4.png'}
+            ];
+            const infos = {id:1, texte: 'coucou les moches'};
+            const titres = {id:1, texte: 'coucou les beaux'};
+            this.randomizeImage(images);
+            document.getElementById('deux').innerHTML = '<p>' + infos.texte + '</p>';
+            document.getElementById('trois').innerHTML = '<p>' + titres.texte + '</p>';
+        }
+    
+        onClick(){
+            let clickEvent = document.getElementById('initParagraphe');
+            clickEvent.onClick = this.actualiseConteneur;
+        }
+    
+        /* on peut pas aller à la ligne dans les box */
+        actualiseConteneur(){
+            let texteun = document.getElementById('textun').value;
+            document.getElementById('deux').innerHTML = '<p>' + texteun + '</p>';
+    
+            let textedeux = document.getElementById('textdeux').value;
+            document.getElementById('trois').innerHTML = '<p>' + textedeux + '</p>';
+        }
+    
+        randomizeImage(images){
+            let randomValue = Math.floor(Math.random() * Math.floor(images.length));
+            document.getElementById('un').innerHTML = '<img src="' + images[randomValue].url + '"/>';
+        }
     }
+    
+    const randomize = new Randomize();
+};
 
-    init(){
-        const images = [
-            {id: 0, url: './images/grimace1.png'},
-            {id: 1, url: './images/grimace2.png'},
-            {id: 2, url: './images/grimace3.png'},
-            {id: 3, url: './images/grimace4.png'}
-        ];
-        const infos = {id:1, texte: 'coucou les moches'};
-        const titres = {id:1, texte: 'coucou les beaux'};
-        this.randomizeImage(images);
-        document.getElementById('deux').innerHTML = '<p>' + infos.texte + '</p>';
-        document.getElementById('trois').innerHTML = '<p>' + titres.texte + '</p>';
-    }
-
-    onClick(){
-        let clickEvent = document.getElementById('initParagraphe');
-        clickEvent.onClick = this.actualiseConteneur;
-    }
-
-    /* on peut pas aller à la ligne dans les box */
-    actualiseConteneur(){
-        let texteun = document.getElementById('textun').value;
-        document.getElementById('deux').innerHTML = '<p>' + texteun + '</p>';
-
-        let textedeux = document.getElementById('textdeux').value;
-        document.getElementById('trois').innerHTML = '<p>' + textedeux + '</p>';
-    }
-
-    randomizeImage(images){
-        let randomValue = Math.floor(Math.random() * Math.floor(images.length));
-        document.getElementById('un').innerHTML = '<img src="' + images[randomValue].url + '"/>';
-    }
-}
-
-const randomize = new Randomize();
