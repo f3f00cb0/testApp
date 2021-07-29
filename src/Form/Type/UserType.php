0e02d37fdcfa23db68788d25f69c6c4f48d5a7dd
Mathieu Mont <mathieu@smile.fr>
2021-07-29 10:47:28 +0200

    security

--- src/Form/Type/UserType.php
+++ src/Form/Type/UserType.php
@@ -5,7 +5,6 @@ namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -17,7 +16,7 @@ class UserType extends AbstractType
     {
         $builder
             ->add('username', TextType::class)
-            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
             ->add('save', SubmitType::class)
         ;
     }
