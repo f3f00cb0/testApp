4841be001067a4acaef8b2d2d0b80ce07394cc5e
Mathieu Mont <mathieu@smile.fr>
2021-07-29 11:09:02 +0200

    security

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -16,6 +16,11 @@ class SecurityController extends AbstractController
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
+
+        if ($this->getUser()) {
+            return $this->redirectToRoute('home');
+        }
+
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
@@ -26,5 +31,13 @@ class SecurityController extends AbstractController
             'last_username' => $lastUsername,
             'error'         => $error,
         ]);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
