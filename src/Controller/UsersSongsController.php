5b6fdd8b6de6b32dcf4e3b845aac1f5387e35a1c
Mathieu Mont <mathieu@smile.fr>
2021-07-30 10:15:42 +0200

    users songs

--- src/Controller/UsersSongsController.php
+++ src/Controller/UsersSongsController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 /**
  * @Route("/users/songs")
@@ -35,6 +36,8 @@ class UsersSongsController extends AbstractController
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form['song']->getData();
+            $file->move('public', $file->getClientOriginalName());
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($usersSong);
             $entityManager->flush();
--- src/Form/UsersSongsType.php
+++ src/Form/UsersSongsType.php
@@ -6,13 +6,15 @@ use App\Entity\UsersSongs;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class UsersSongsType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('song')
+            ->add('song', FileType::class)
             ->add('user')
         ;
     }
