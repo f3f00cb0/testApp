3a8b41ae24828ed81171b8d73d346251d33edcb8
Mathieu Mont <mathieu@smile.fr>
2021-08-30 16:45:52 +0200

    Ajout d'album

--- src/Form/AlbumType.php
+++ src/Form/AlbumType.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\Album;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\OptionsResolver;use Symfony\Component\Validator\Constraints\File;
 
 class AlbumType extends AbstractType
 {
@@ -14,7 +16,19 @@ class AlbumType extends AbstractType
         $builder
             ->add('title')
             ->add('artiste')
-            ->add('cover')
+            ->add('cover', FileType::class, [
+                'label' => 'couverture',
+                'mapped' => false,
+                'required' => false,
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                    ])
+                ],
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Créer Album'
+            ])
         ;
     }
 
