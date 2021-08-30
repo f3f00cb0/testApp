492c906e9bad9b184153d1b8d0713edce4bd6a7e
Mathieu Mont <mathieu@smile.fr>
2021-08-30 16:49:41 +0200

    Ajout d'album

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -9,6 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @Route("/album")
@@ -28,7 +29,7 @@ class AlbumController extends AbstractController
     /**
      * @Route("/new", name="album_new", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    public function new(Request $request, SluggerInterface $slugger): Response
     {
         $album = new Album();
         $form = $this->createForm(AlbumType::class, $album);
@@ -36,6 +37,22 @@ class AlbumController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
+            $cover = $form['cover']->getData();
+            $originalFilename = pathinfo($cover->getClientOriginalName(), PATHINFO_FILENAME);
+            $safeFilename = $slugger->slug($originalFilename);
+            $newFilename = $safeFilename.'-'.uniqid().'.'.$cover->guessExtension();
+
+            try {
+                $cover->move(
+                    'userssongs/',
+                    $newFilename
+                );
+            } catch (FileException $e) {
+                // ... handle exception if something happens during file upload
+            }
+
+            $album->setCover($newFilename);
+
             $entityManager->persist($album);
             $entityManager->flush();
 
