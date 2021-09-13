ff5ebcbcc9c2b65455ec3881de085434c572b5f1
Mathieu Mont <mathieu.mont@smile.fr>
2021-09-13 15:26:34 +0200

    Formulaire ajout album

--- src/Form/AlbumType.php
+++ src/Form/AlbumType.php
@@ -2,12 +2,14 @@
 
 namespace App\Form;
 
+use App\Entity\Artist;
 use App\Entity\Album;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;use Symfony\Component\Validator\Constraints\File;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class AlbumType extends AbstractType
 {
@@ -15,7 +17,10 @@ class AlbumType extends AbstractType
     {
         $builder
             ->add('title')
-            ->add('artiste')
+            ->add('artiste', EntityType::class, [
+                'class' => Artist::class,
+                'choice_label' => 'name'
+            ])
             ->add('cover', FileType::class, [
                 'label' => 'couverture',
                 'mapped' => false,
